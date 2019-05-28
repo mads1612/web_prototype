1bf5e6d9ca0e935bbb3ecc656086d517029bd957
Mads Iversen <madsiversen@hotmail.com>
2019-05-28 19:15:10 +0200

    Et commit

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-function
+window.addEventListener("", );
+
+function sidenVises() {
+    console.log("");
+
+    document.querySelector("#").addEventListener("", );
+
+
+}
+
+
+function toggleMenu() {
+    console.log("");
+    document.querySelector("#").classList.toggle("");
+
+    let erSkjult = document.querySelector("#").classList.contains("");
+
+    if (erSkjult == ) {
+        document.querySelector("#").textContent = "☰";
+    } else {
+        document.querySelector("#").textContent = "X";
+    }
+}

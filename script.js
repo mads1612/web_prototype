7652eb0599f225d70ee5f1ee1be4c8d33cbd9e3d
Mads Iversen <madsiversen@hotmail.com>
2019-05-31 11:54:23 +0200

    lavet en function som skal hide disclaimer box når man trykker på knappen

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function toggleMenu() {
     var element = document.getElementById("menu");
-    element.classList.toggle("menu_hidden");
+    element.classList.toggle("hide");
+}
+
+function hideCookieDisclaimer() {
+    var element = document.getElementById("cookie-disclaimer");
+    element.classList.toggle("hide");
 }
